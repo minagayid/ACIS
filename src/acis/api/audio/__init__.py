37acfd1aae46e83bf58428a3c6c0edd54b22131a
minagayid <minagayid@gmail.com>
2026-09-15 01:30:23 +0300

    Remove misplaced package marker

--- src/acis/api/audio/__init__.py
+++
@@ -1,4 +0,0 @@
-from .features import FEATURE_NAMES, acoustic_vector, cosine_similarity
-from .validate import validate_wav
-
-__all__ = ["FEATURE_NAMES", "acoustic_vector", "cosine_similarity", "validate_wav"]
