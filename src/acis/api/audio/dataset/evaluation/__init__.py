03550b4f60e474cb439a3a4bcfbd15516ca91432
minagayid <minagayid@gmail.com>
2026-09-15 01:30:04 +0300

    Remove misplaced package marker

--- src/acis/api/audio/dataset/evaluation/__init__.py
+++
@@ -1,4 +0,0 @@
-from .metrics import evaluate_predictions, majority_class_accuracy
-from .reports import render_report
-
-__all__ = ["evaluate_predictions", "majority_class_accuracy", "render_report"]
