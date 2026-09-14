2a798099fc957e9153ecf75e31f03a74dfc59ef2
minagayid <minagayid@gmail.com>
2026-09-15 01:23:01 +0300

    Add evaluation package marker

---
+++ src/acis/api/audio/dataset/evaluation/__init__.py
@@ -0,0 +1,4 @@
+from .metrics import evaluate_predictions, majority_class_accuracy
+from .reports import render_report
+
+__all__ = ["evaluate_predictions", "majority_class_accuracy", "render_report"]
