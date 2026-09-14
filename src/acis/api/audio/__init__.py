1e0425026f940ca208277154b1ca70c8851e45ea
minagayid <minagayid@gmail.com>
2026-09-15 01:22:38 +0300

    Add audio package marker

---
+++ src/acis/api/audio/__init__.py
@@ -0,0 +1,4 @@
+from .features import FEATURE_NAMES, acoustic_vector, cosine_similarity
+from .validate import validate_wav
+
+__all__ = ["FEATURE_NAMES", "acoustic_vector", "cosine_similarity", "validate_wav"]
