80489ebae99b52c1cf1fa6dc3edd33325601e974
minagayid <minagayid@gmail.com>
2026-09-15 01:29:03 +0300

    Remove misplaced package marker

--- src/acis/api/audio/dataset/evaluation/ingest/models/retrieval/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from .annotation import Annotation, AnnotationSegment
-from .prediction import Prediction, RetrievedExample
-from .recording import Recording
-
-__all__ = ["Annotation", "AnnotationSegment", "Prediction", "Recording", "RetrievedExample"]
