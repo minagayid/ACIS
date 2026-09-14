e1b2ee12328887124f2ee5ba174657e73df661a9
minagayid <minagayid@gmail.com>
2026-09-15 01:23:32 +0300

    Add schemas package marker

---
+++ src/acis/api/audio/dataset/evaluation/ingest/models/retrieval/schemas/__init__.py
@@ -0,0 +1,5 @@
+from .annotation import Annotation, AnnotationSegment
+from .prediction import Prediction, RetrievedExample
+from .recording import Recording
+
+__all__ = ["Annotation", "AnnotationSegment", "Prediction", "Recording", "RetrievedExample"]
